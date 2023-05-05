7b5d7f63c86d91d8fc7b00af6dcf57c0725ba9e0
unknown <1chudik1@mail.ru>
2023-05-05 12:30:26 +0600

    v1

---
+++ eth.py
@@ -0,0 +1,37 @@
+
+from typing import Optional
+from hdwallet import BIP44HDWallet
+from hdwallet.cryptocurrencies import EthereumMainnet
+from hdwallet.derivations import BIP44Derivation
+from hdwallet.utils import generate_mnemonic
+import openpyxl
+
+book = openpyxl.Workbook()
+sheet = book.active
+sheet['A1'] = "Mnemonic"
+sheet['B1'] = "Address"
+sheet['C1'] = "Private key"
+
+# Number of wallets
+N = int(input("Enter number of wallets: "))
+
+for i in range(N):
+    MNEMONIC: str = generate_mnemonic(language="english", strength=128)
+    PASSPHRASE: Optional[str] = None
+
+    bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
+    bip44_hdwallet.from_mnemonic(
+        mnemonic=MNEMONIC, language="english", passphrase=PASSPHRASE
+    )
+    bip44_hdwallet.clean_derivation()
+    sheet.cell(row=i + 2, column=1).value = bip44_hdwallet.mnemonic()
+    bip44_derivation: BIP44Derivation = BIP44Derivation(
+        cryptocurrency=EthereumMainnet, account=0, change=False, address=0
+    )
+    bip44_hdwallet.from_path(path=bip44_derivation)
+    sheet.cell(row=i + 2, column=2).value = bip44_hdwallet.address()
+    sheet.cell(row=i + 2, column=3).value = bip44_hdwallet.private_key()
+    bip44_hdwallet.clean_derivation()
+
+book.save("D:/eth.accs/MM.xlsx")
+book.close()
